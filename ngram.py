763a590cb58b2e5b61c87472525566db1c9bf43e
jordanimayer <isaacmayer42@gmail.com>
2019-05-29 13:34:36 -0400

    begin developing homemade ngram model

---
+++ ngram.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""
+Use n-gram learning model to attempt to reproduce language.
+
+@author: Jordan Mayer, isaacmayer42@gmail.com
+@version: 05.29.2019
+"""
+
+def words_list(file):
+  """
+  Read file and return list of words in sequential order.
+
+  Input:
+    file: file in read mode
+
+  Return:
+    List of words in sequential order
+  """
+
+
+def parse_text(filename, n):
+  """
+  Parse text file for necessary frequencies.
+
+  Input:
+    filename: path to .txt file as string
+    n: number of sequential words to take into account (n-gram model)
+
+  Return:
+    dictionary of dictionaries of form
+      {"word" : {"previous sequence": count(word | previous sequence)}}
+  """
+
+
+
+if __name__ == "__main__":
+  """
+  TODO: main function
+  """
+  pass
